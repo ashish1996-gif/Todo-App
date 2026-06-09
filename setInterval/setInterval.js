287f8393db68e4b0039cae51518b7348ed7b8e47
Ashish Mathur <mathur.ashish33@gmail.com>
2026-06-09 18:13:54 +0530

    Updated Todo App

---
+++ setInterval/setInterval.js
@@ -0,0 +1,28 @@
+// // let count = 100;
+
+// // setInterval(function(){
+
+// //    count++;
+
+// //    document.getElementById("count")
+// //    .innerText = count;
+
+// // },1000);
+
+
+
+
+
+// function login(callback){
+
+//    console.log("Checking user...");
+
+//    callback();
+
+// }
+
+// login(function(){
+
+//    console.log("Login Success");
+
+// });
